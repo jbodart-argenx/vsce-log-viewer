79f9d1fa0ee6197728c845062c60c16b00b7a923
Jean-Michel Bodart <jbodart@argenx.com>
2024-12-11 16:56:21 +0100

    Update extension.js
    Increase logging
    Add error handling
    Rename commands "vsce-log-viewer.scanLogFile" to "jbodart-argenx-log-viewer.scanLogFile" and "vsce-log-viewer.clearLogDiagnostics" to "jbodart-argenx-log-viewer.clearLogDiagnostics"

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,20 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as readline from "readline";
+import { PassThrough } from 'stream';
 
 export function activate(context: vscode.ExtensionContext) {
    const diagnosticCollection =
       vscode.languages.createDiagnosticCollection("logDiagnostics");
    context.subscriptions.push(diagnosticCollection);
 
-   const scanLogFile = (document: vscode.TextDocument) => {
+   const scanLogFile = async (document: vscode.TextDocument) => {
       if (document.languageId !== "log") {
          return;
       }
 
-      const filePath = document.uri.fsPath;
       const diagnostics: vscode.Diagnostic[] = [];
-
+      
       const errorRegex = /^ERROR(?::|\d+-\d+)\s*(.*)$/;
       const warningRegex = /^WARNING:\s*(.*)$/;
 		const infoRegex = /^(NOTE:\s*.*)$/;
@@ -26,122 +26,176 @@ export function activate(context: vscode.ExtensionContext) {
 		const hintRegex = /^((?:INFO|Notice):\s+.*)$/;
       const continuationRegex = /^\s+(.*)$/;
 
-      const rl = readline.createInterface({
-         input: fs.createReadStream(filePath),
-         crlfDelay: Infinity,
-      });
-
-      let lineNumber = 0;
-      let currentMessage: string | null = null;
-      let currentSeverity: vscode.DiagnosticSeverity | null = null;
-      let startLineNumber: number | null = null;
-      let lineLength: number = 0;
-
-      rl.on("line", (line) => {
-         let match;
-         if ((match = errorRegex.exec(line))) {
-            if (currentMessage) {
-               const range = new vscode.Range(
-                  startLineNumber!,
-                  0,
-                  lineNumber - 1,
-                  lineLength  || line.length
-               );
-               const diagnostic = new vscode.Diagnostic(
-                  range,
-                  currentMessage,
-                  currentSeverity!
-               );
-                              diagnostics.push(diagnostic);
-               diagnosticCollection.set(document.uri, diagnostics);
-            }
-            currentMessage = match[1];
-            currentSeverity = vscode.DiagnosticSeverity.Error;
-            startLineNumber = lineNumber;
-            lineLength = line.length;
-         } else if ((match = warningRegex.exec(line))) {
-            if (currentMessage) {
-               const range = new vscode.Range(
-                  startLineNumber!,
-                  0,
-                  lineNumber - 1,
-                  lineLength  || line.length
-               );
-               const diagnostic = new vscode.Diagnostic(
-                  range,
-                  currentMessage,
-                  currentSeverity!
-               );
-               diagnostics.push(diagnostic);
-               diagnosticCollection.set(document.uri, diagnostics);
-            }
-            currentMessage = match[1];
-            currentSeverity = vscode.DiagnosticSeverity.Warning;
-            startLineNumber = lineNumber;
-            lineLength = line.length;
-         } else if ((match = infoRegex.exec(line))) {
-            if (currentMessage) {
-               const range = new vscode.Range(
-                  startLineNumber!,
-                  0,
-                  lineNumber - 1,
-                  lineLength  || line.length
-               );
-               const diagnostic = new vscode.Diagnostic(
-                  range,
-                  currentMessage,
-                  currentSeverity!
-               );
-               diagnostics.push(diagnostic);
-               diagnosticCollection.set(document.uri, diagnostics);
-            }
-            currentMessage = match[1];
-            if (issueRegEx.test(line)) {
-               // currentSeverity = vscode.DiagnosticSeverity.Information;
-               currentSeverity = vscode.DiagnosticSeverity.Information;
-            // } else if (libRegEx.test(line) || fileRegEx.test(line)) {
-            //    currentSeverity = vscode.DiagnosticSeverity.Information;
-            } else {
-               currentSeverity = vscode.DiagnosticSeverity.Hint;
-               // currentSeverity = vscode.DiagnosticSeverity.Information;
-            }
-            startLineNumber = lineNumber;
-            lineLength = line.length;
-         } else if ((match = hintRegex.exec(line))) {
-            if (currentMessage) {
-               const range = new vscode.Range(
-                  startLineNumber!,
-                  0,
-                  lineNumber - 1,
-                  lineLength  || line.length
-               );
-               const diagnostic = new vscode.Diagnostic(
-                  range,
-                  currentMessage,
-                  currentSeverity!
-               );
-               diagnostics.push(diagnostic);
-               diagnosticCollection.set(document.uri, diagnostics);
-            }
-            currentMessage = match[1];
-            // if (/^Notice:/.test(line)) {
-            //    currentSeverity = vscode.DiagnosticSeverity.Information;
-            // } else {
-               currentSeverity = vscode.DiagnosticSeverity.Hint;
-            // }
-            startLineNumber = lineNumber;
-            lineLength = line.length;
-         } else if ((match = continuationRegex.exec(line))) {
-            if (currentMessage) {
-               currentMessage += "\n" + match[1];
-            }
+      let errors = 0;
+      let warnings = 0;
+      let notesIssues = 0;
+      let filePath :String = '<not assigned>';
+      
+      let rl;
+      try {
+         if (document.uri.scheme === 'file') {
+            filePath = document.uri.fsPath;
+            console.warn(`(log-viewer) scanLogFile starting Scan of file: ${filePath}`);
+            rl = readline.createInterface({ input: fs.createReadStream(document.uri.fsPath), crlfDelay: Infinity, });
          } else {
+            filePath = `${document.uri}`;
+            console.warn(`(log-viewer) scanLogFile starting Scan of file: ${filePath}`);
+            const fileData = await vscode.workspace.fs.readFile(document.uri);
+            const passThrough = new PassThrough();
+            passThrough.end(fileData);
+            rl = readline.createInterface({
+               input: passThrough,
+               crlfDelay: Infinity
+            });
+         }
+
+         let lineNumber = 0;
+         let currentMessage: string | null = null;
+         let currentSeverity: vscode.DiagnosticSeverity | null = null;
+         let startLineNumber: number | null = null;
+         let lineLength: number = 0;
+
+
+         rl.on("line", (line) => {
+            let match;
+            if ((match = errorRegex.exec(line))) {
+               if (currentMessage) {
+                  const range = new vscode.Range(
+                     startLineNumber!,
+                     0,
+                     lineNumber - 1,
+                     lineLength  || line.length
+                  );
+                  const diagnostic = new vscode.Diagnostic(
+                     range,
+                     currentMessage,
+                     currentSeverity!
+                  );
+                                 diagnostics.push(diagnostic);
+                  diagnosticCollection.set(document.uri, diagnostics);
+               }
+               currentMessage = match[1];
+               currentSeverity = vscode.DiagnosticSeverity.Error;
+               errors++;
+               startLineNumber = lineNumber;
+               lineLength = line.length;
+            } else if ((match = warningRegex.exec(line))) {
+               if (currentMessage) {
+                  const range = new vscode.Range(
+                     startLineNumber!,
+                     0,
+                     lineNumber - 1,
+                     lineLength  || line.length
+                  );
+                  const diagnostic = new vscode.Diagnostic(
+                     range,
+                     currentMessage,
+                     currentSeverity!
+                  );
+                  diagnostics.push(diagnostic);
+                  diagnosticCollection.set(document.uri, diagnostics);
+               }
+               currentMessage = match[1];
+               currentSeverity = vscode.DiagnosticSeverity.Warning;
+               warnings++;
+               startLineNumber = lineNumber;
+               lineLength = line.length;
+            } else if ((match = infoRegex.exec(line))) {
+               if (currentMessage) {
+                  const range = new vscode.Range(
+                     startLineNumber!,
+                     0,
+                     lineNumber - 1,
+                     lineLength  || line.length
+                  );
+                  const diagnostic = new vscode.Diagnostic(
+                     range,
+                     currentMessage,
+                     currentSeverity!
+                  );
+                  diagnostics.push(diagnostic);
+                  diagnosticCollection.set(document.uri, diagnostics);
+               }
+               currentMessage = match[1];
+               if (issueRegEx.test(line)) {
+                  currentSeverity = vscode.DiagnosticSeverity.Information;
+                  notesIssues++;
+               } else {
+                  currentSeverity = vscode.DiagnosticSeverity.Hint;
+               }
+               startLineNumber = lineNumber;
+               lineLength = line.length;
+            } else if ((match = hintRegex.exec(line))) {
+               if (currentMessage) {
+                  const range = new vscode.Range(
+                     startLineNumber!,
+                     0,
+                     lineNumber - 1,
+                     lineLength  || line.length
+                  );
+                  const diagnostic = new vscode.Diagnostic(
+                     range,
+                     currentMessage,
+                     currentSeverity!
+                  );
+                  diagnostics.push(diagnostic);
+                  diagnosticCollection.set(document.uri, diagnostics);
+               }
+               currentMessage = match[1];
+               currentSeverity = vscode.DiagnosticSeverity.Hint;
+               startLineNumber = lineNumber;
+               lineLength = line.length;
+            } else if ((match = continuationRegex.exec(line))) {
+               if (currentMessage) {
+                  currentMessage += "\n" + match[1];
+               }
+            } else {
+               if (currentMessage) {
+                  const range = new vscode.Range(
+                     startLineNumber!,
+                     0,
+                     lineNumber - 1,
+                     lineLength  || line.length
+                  );
+                  const diagnostic = new vscode.Diagnostic(
+                     range,
+                     currentMessage,
+                     currentSeverity!
+                  );
+                  diagnostics.push(diagnostic);
+                  diagnosticCollection.set(document.uri, diagnostics);
+                  currentMessage = null;
+                  currentSeverity = null;
+                  startLineNumber = null;
+                  lineLength = 0;
+               }
+            }
+            lineNumber++;
+         });
+
+         rl.on("close", () => {
+            console.warn(`(log-viewer) scanLogFile successfully completed reading log file "${filePath}"`);
+            if (errors) {
+               console.error(`(log-viewer) scanLogFile Fond ${errors} errors.`);
+            } else {
+               console.log(`(log-viewer) scanLogFile Fond ${errors} errors.`);
+            }
+            if (warnings) {
+               console.warn(`(log-viewer) scanLogFile Fond ${warnings} warnings.`);
+            } else {
+               console.log(`(log-viewer) scanLogFile Fond ${warnings} warnings.`);
+            }
+            if (notesIssues) {
+               console.warn(`(log-viewer) scanLogFile Fond ${notesIssues} notes issues.`);
+            } else {
+               console.log(`(log-viewer) scanLogFile Fond ${notesIssues} notes issues.`);
+            }
             if (currentMessage) {
                const range = new vscode.Range(
                   startLineNumber!,
                   0,
                   lineNumber - 1,
-                  lineLength  || line.length
+                  0
                );
                const diagnostic = new vscode.Diagnostic(
                   range,
@@ -150,32 +204,15 @@ export function activate(context: vscode.ExtensionContext) {
                );
                diagnostics.push(diagnostic);
                diagnosticCollection.set(document.uri, diagnostics);
-               currentMessage = null;
-               currentSeverity = null;
-               startLineNumber = null;
-               lineLength = 0;
             }
-         }
-         lineNumber++;
-      });
-
-      rl.on("close", () => {
-         if (currentMessage) {
-            const range = new vscode.Range(
-               startLineNumber!,
-               0,
-               lineNumber - 1,
-               0
-            );
-            const diagnostic = new vscode.Diagnostic(
-               range,
-               currentMessage,
-               currentSeverity!
-            );
-            diagnostics.push(diagnostic);
-            diagnosticCollection.set(document.uri, diagnostics);
-         }
-      });
+         });
+         
+      } catch (error :any) {
+         console.error(`(log-viewer) scanLogFile Error reading log file "${filePath}": ${error.message}`);
+         console.error(`(log-viewer) scanLogFile Fond ${errors} errors`);
+         console.error(`(log-viewer) scanLogFile Fond ${warnings} warnings`);
+         console.error(`(log-viewer) scanLogFile Fond ${notesIssues} notes issues`);
+      }
    };
 
 	const clearDiagnostics = () => {
@@ -183,29 +220,43 @@ export function activate(context: vscode.ExtensionContext) {
 	};
 
 	// Automatically scan log file opened in editor
-   vscode.workspace.onDidOpenTextDocument(scanLogFile);
+   vscode.workspace.onDidOpenTextDocument(async (document) => {
+      try {
+         await scanLogFile(document);
+      } catch(err :any) {
+         console.error(`scanLogFile error: ${err.message}`);
+      }
+   });
 
    // Clear the diagnostics from the Problems panel when the .log file is closed
    vscode.workspace.onDidCloseTextDocument(clearDiagnostics);
 
    // Trigger a new scan whenever the active editor changes to a new log file
-   vscode.window.onDidChangeActiveTextEditor((editor) => {
+   vscode.window.onDidChangeActiveTextEditor(async (editor) => {
       if (editor && editor.document.languageId === 'log') {
-         scanLogFile(editor.document);
+         try {
+            await scanLogFile(editor.document);
+         } catch(err :any) {
+            console.error(`scanLogFile error: ${err.message}`);
+         }   
       }
    });
 
    context.subscriptions.push(
-      vscode.commands.registerCommand("vsce-log-viewer.scanLogFile", () => {
+      vscode.commands.registerCommand("jbodart-argenx-log-viewer.scanLogFile", async () => {
          const editor = vscode.window.activeTextEditor;
          if (editor) {
-            scanLogFile(editor.document);
+            try {
+               await scanLogFile(editor.document);
+            } catch(err :any) {
+               console.error(`scanLogFile error: ${err.message}`);
+            }
          }
       })
    );
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('vsce-log-viewer.clearLogDiagnostics', clearDiagnostics)
+		vscode.commands.registerCommand('jbodart-argenx-log-viewer.clearLogDiagnostics', clearDiagnostics)
 	);
 }
 
